af01d682142ab5dc640a19862fea90ddd54a3b45
Austin Barrett <virus.cmd@gmail.com>
2017-12-02 15:37:06 -0500

    tweaks

--- getNetworks.sh
+++ getNetworks.sh
@@ -2,6 +2,7 @@
 # Config Options
 set -e
 source ./log.sh
+source ./isroot.sh
 
 # VARIABLES
 
@@ -10,12 +11,6 @@ source ./log.sh
 function scan {
     iwlist $1 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID' | sed 's/ - /\n/g' | sed -E 's/^\s*?/\n/g' | egrep -v '^$'
 }
-
-# Checking to make sure script is run as root
-if [[ $EUID -ne 0 ]]; then
-   log err "This script must be run as root" 
-   exit 1
-fi
 
 #####################################
 # MAIN
