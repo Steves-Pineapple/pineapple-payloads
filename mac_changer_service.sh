afcce63724eecf37d2524d2d9038c5cd1c48089f
Austin Barrett <virus.cmd@gmail.com>
2017-12-03 11:19:06 -0500

    tweaks

--- mac_changer_service.sh
+++ mac_changer_service.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+# /etc/init.d/macChanger
+
+### BEGIN INIT INFO
+# Provides:          macChanger
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Simple script to start a program at boot
+### END INIT INFO
+
+
 
 # Config Option
 set -e 
@@ -7,11 +17,20 @@ source /opt/wifipineapple/payloads/macAddressToRandom.sh
 # Variables
 readonly interfaceToChange=$(cat /opt/wifipineapple/config/macAddressInterface)
 
-#####################################
-# MAIN
-#####################################
-[[ -z "$(ifconfig $interfaceToChange)" ]] ||  {
-    log err "Interface $interfaceToChange is not currently up"
+case "$1" in
+  start)
+    echo "Starting noip"
+    # run application you want to start
+    [[ -z "$(ifconfig $interfaceToChange)" ]] ||  {
+        log err "Interface $interfaceToChange is not currently up"
+        exit 1
+    }
+    configureMacAddress $interfaceToChange || log err "could not randomise mac address"
+    ;;
+  *)
+    echo "Usage: /etc/init.d/noip {start}"
     exit 1
-}
-configureMacAddress $interfaceToChange || log err "could not randomise mac address"
+    ;;
+esac
+
+exit 0
