e6d2990c754ccaba69d741ddaed4d28ee9873cda
Austin Barrett <virus.cmd@gmail.com>
2017-12-07 18:48:29 -0500

    testing

--- bin/macchanger.sh
+++ bin/macchanger.sh
@@ -2,13 +2,13 @@
 
 #  Config
 readonly scriptBasename=$(basename $0 | rev | cut -c4- | rev)
-source ../payload/helper.sh
+source $(dirname $(dirname $0))/payload/helper.sh
 
 if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
     readManual $scriptBasename
     exit 
 fi
 
-source ../payload/macAddressToRandom.sh
+source $(dirname $(dirname $0))/payload/macAddressToRandom.sh
 
 configureMacAddress $1 $2
