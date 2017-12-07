a65d0d8b0cfff78fdc73e8ae9842fd0e77c655f7
Austin Barrett <virus.cmd@gmail.com>
2017-12-07 18:14:08 -0500

    testing

--- bin/pineapple-help.sh
+++ bin/pineapple-help.sh
@@ -2,10 +2,7 @@
 
 #  Config
 readonly scriptBasename=$(basename $0 | rev | cut -c4- | rev)
-source ../payload/helper.sh
-ls ../payload/
-ls ../
-ls ..
+source ./payload/helper.sh
 
 if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
     readManual $scriptBasename
