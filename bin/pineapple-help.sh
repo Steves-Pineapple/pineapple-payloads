f3a2882acfbc753761d765d18167664885a40459
Austin Barrett <virus.cmd@gmail.com>
2017-12-06 19:15:43 -0500

    tweaks

---
+++ bin/pineapple-help.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#  Config
+readonly scriptBasename=$(basename $0 | rev | cut -c4- | rev)
+source ../payload/helper.sh
+
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+    readManual $scriptBasename
+    exit 
+fi
+
