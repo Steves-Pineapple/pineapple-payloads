7dce9a0266d237af679da5b424b17295187d723e
Austin Barrett <virus.cmd@gmail.com>
2017-12-07 22:36:44 -0500

    testing

--- payload/getNetworks.sh
+++ payload/getNetworks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Config Options
 set -e
-source $(dirname $(dirname $0))/log.sh
-source $(dirname $(dirname $0))/isroot.sh
+source $(dirname $0)/log.sh
+source $(dirname $0)/isroot.sh
 
 # VARIABLES
 
