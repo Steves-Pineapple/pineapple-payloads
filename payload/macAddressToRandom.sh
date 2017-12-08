66e6eb9a62b1a0fd9c35ffcabb6c098820861c6f
Austin Barrett <virus.cmd@gmail.com>
2017-12-07 23:32:57 -0500

    testing

--- payload/macAddressToRandom.sh
+++ payload/macAddressToRandom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Config Options
-source $(dirname $(dirname $0))/payload/log.sh
+source ../payload/log.sh
 # source $(dirname $(dirname $0))/payload/isroot.sh
 
 # Functions
@@ -10,7 +10,6 @@ function generateMacAddress {
 }
 
 function configureMacAddress {
-    echo HERE 5
     ifconfig $1 hw ether $2
 }
 
@@ -19,13 +18,10 @@ function configureMacAddress {
 #####################################
 if [[ ! -z "$1" ]]; then 
     if [[ ! -z "$2" ]]; then
-        echo HERE 1
         configureMacAddress $1 $2 || log err "could not change MAC Address for $1 to $2"
     else
-        echo HERE 2
         echo configureMacAddress $1 $(generateMacAddress)
         configureMacAddress $1 $(generateMacAddress) #|| log err "could not change MAC Address for $1"
     fi
-    echo HERE 
     ifconfig -a > /dev/null # this is required for the address to be changable at  later date.
 fi
